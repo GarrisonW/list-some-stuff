3321a549632f883ba81fb4e39896174f71d7ea7b
Garrison Wright <wrightnow99@gmail.com>
2017-10-29 17:26:15 -0500

    Expanded hardcoded list and added styling for list items.

--- App.js
+++ App.js
@@ -5,9 +5,15 @@ export default class App extends React.Component {
   render() {
     return (
       <FlatList
-        data={[{key: 'a', name: 'Fred'}, {key: 'b', name: 'Velma'}]}
-        renderItem={({item}) => <View>
-                                    <Text>{item.key}</Text>
+        data={[{key: 'a', name: 'Fred'},
+               {key: 'b', name: 'Scooby'},
+               {key: 'c', name: 'Daphnie'},
+               {key: 'd', name: 'Shaggy'},
+               {key: 'e', name: 'Ghost'},
+               {key: 'f', name: 'Velma'}]
+             }
+        renderItem={({item}) => <View style={{height: 100}}>
+                                    <Text style={styles.red}>{item.key}</Text>
                                     <Text style={styles.bigblue}>{item.name}</Text>
                                 </View>}
       />
@@ -20,8 +26,9 @@ const styles = StyleSheet.create({
     color: 'blue',
     fontWeight: 'bold',
     fontSize: 30,
+    backgroundColor: 'red'
   },
-  red: {
+  redText: {
     color: 'red',
   },
 });
